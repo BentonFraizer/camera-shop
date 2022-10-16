f3ddac2628a47ae3826364dd5aba3862b2245451
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-10-16 13:40:19 +0300

    Получены данные по продукту, реализованы хлебные крошки - даныне в табы записываются, но сами табы пока не реализованы

--- project/src/store/site-data/site-data.ts
+++ project/src/store/site-data/site-data.ts
@@ -1,21 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { NameSpace } from '../../consts';
 import { SiteData } from '../../types/state';
-import { fetchCamerasAction, fetchPromoCameraAction } from '../api-actions';
+import { fetchCamerasAction, fetchCameraAction, fetchPromoCameraAction } from '../api-actions';
 
 const initialState: SiteData = {
   camerasList: [],
+  camera: null,
   promoCamera: null,
 };
 
 export const siteData = createSlice({
   name: NameSpace.Data,
   initialState,
-  reducers: {},
+  reducers: {
+    resetCameraData: (state) => {
+      state.camera = null;
+    }
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchCamerasAction.fulfilled, (state, action) => {
         state.camerasList = action.payload;
+      })
+      .addCase(fetchCameraAction.fulfilled, (state, action) => {
+        state.camera = action.payload;
       })
       .addCase(fetchPromoCameraAction.fulfilled, (state, action) => {
         state.promoCamera = action.payload;
@@ -23,3 +31,4 @@ export const siteData = createSlice({
   },
 });
 
+export const { resetCameraData } = siteData.actions;
